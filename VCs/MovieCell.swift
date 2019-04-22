b505798c5d40da1ff3bb5bcee04400dbdb08de7e
Guy Ardito <guyardito@gmail.com>
2019-04-22 09:28:17 -0400

    add  custom cell for the MovieList_VC table

---
+++ VCs/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  MovieDBDemo
+//
+//  Created by Guy (main) on 4/22/19.
+//  Copyright © 2019 Rich Life LLC. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
