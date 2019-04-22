373a60b176242e30530e82609c56b531e0603aed
Guy Ardito <guyardito@gmail.com>
2019-04-22 10:02:30 -0400

    add  outlets for the controls

--- VCs/MovieCell.swift
+++ VCs/MovieCell.swift
@@ -9,16 +9,23 @@
 import UIKit
 
 class MovieCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+	
+	@IBOutlet var title:UILabel?
+	@IBOutlet var posterPath:UILabel?
+	@IBOutlet var overview:UILabel?
+	@IBOutlet var poster:UIImageView?
+	
+	
+	
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		// Initialization code
+	}
+	
+	override func setSelected(_ selected: Bool, animated: Bool) {
+		super.setSelected(selected, animated: animated)
+		
+		// Configure the view for the selected state
+	}
+	
 }
